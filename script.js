d9dc3a8f391d2666af99df7940146bff35ef2744
nedicmilann <nedicmilan3@gmail.com>
2025-03-09 10:41:52 +0100

    dodavanje nove logike i funkcionalnosti u js

--- script.js
+++ script.js
@@ -11,9 +11,17 @@ const diceEl = document.querySelector('.dice');
 const buttonNew = document.querySelector('.btn--new');
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
-let player0Name = document.getElementById('name--0');
-let player1Name = document.getElementById('name--1');
+const player0Name = document.getElementById('name--0');
+const player1Name = document.getElementById('name--1');
+const winnerModal = document.querySelector('.modal');
+const winnerModalBtn = document.querySelector('.btn--close');
+const winnerModalOverlay = document.querySelector('.overlay');
+const modalText = document.querySelector('.winner-text');
+const playerNameModal = document.querySelector('.modal-name');
+const playerNameBtn = document.querySelector('.btn--start');
+const playerNameModalOverlay = document.querySelector('.overlay');
 
+//Variables:
 let scores, currentScore, activePlayer, playing;
 
 //Starting conditions
@@ -21,7 +29,6 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-//Initial game settings
 const init = function () {
   scores = [0, 0];
   currentScore = 0;
@@ -40,19 +47,34 @@ init();
 
 //Player names:
 const names = function () {
-  let player0P = prompt('Unesite ime prvog igraca');
+  let player0P = document.getElementById('player0-name').value.trim();
   player0Name.textContent = player0P;
-  let player1P = prompt('Unesite ime drugog igraca');
+  let player1P = document.getElementById('player1-name').value.trim();
   player1Name.textContent = player1P;
 };
-names();
 
+//Switching players logic:
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   currentScore = 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
+};
+
+//Winner modal:
+const modalWinner = function () {
+  if (activePlayer === 0) {
+    modalText.textContent = `🎉${player0Name.textContent} IS WINNER !!!🎉`;
+  } else {
+    modalText.textContent = `🎉${player1Name.textContent} IS WINNER !!!🎉`;
+  }
+  winnerModal.classList.remove('hidden');
+  winnerModalOverlay.classList.remove('hidden');
+  winnerModalBtn.addEventListener('click', function () {
+    winnerModal.classList.add('hidden');
+    winnerModalOverlay.classList.add('hidden');
+  });
 };
 
 //Rolling dice functionality
@@ -75,6 +97,14 @@ buttonRoll.addEventListener('click', function () {
   }
 });
 
+//Setting player names and starting game:
+playerNameBtn.addEventListener('click', function () {
+  playerNameModal.classList.add('hidden');
+  playerNameModalOverlay.classList.add('hidden');
+  names();
+});
+
+//ROLL button logic:
 buttonHold.addEventListener('click', function () {
   if (playing) {
     //1. Add current score to active player
@@ -93,6 +123,7 @@ buttonHold.addEventListener('click', function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
       diceEl.classList.add('hidden');
+      modalWinner();
     } else {
       //Switch to the next player
       switchPlayer();
@@ -100,4 +131,5 @@ buttonHold.addEventListener('click', function () {
   }
 });
 
+//Starting new game:
 buttonNew.addEventListener('click', init);
